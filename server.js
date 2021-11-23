57bab44603fb05f81101d03dd1fcd945bc2ae813
Emanuel Ceballos <eceballos@matechstudios.com>
2021-11-23 19:34:17 -0300

    Scaffold setup

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+// const users = require('./routes/users');
+// const auth = require('./routes/auth');
+const db = require('./db/db_connection');
+
+db.connect();
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({
+    extended: true
+}));
+
+/* Routes Setup */
+
+// app.use('/api/users', users);
+// app.use('/api/auth', auth);
+
+/* End Routes */
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log('API REST up & running');
+});
